3d20725d0380ab1abb20d71479d816bac11518a8
LisethPerez <liseth.perez@uptc.edu.co>
2020-02-17 16:30:35 -0500

    Test manage bovines

--- PRODULEC/produlect/tests.py
+++ PRODULEC/produlect/tests.py
@@ -5,15 +5,43 @@ from produlect.models import *
 class TestViews(TestCase):
     def setUp(self):
         self.client = Client()
-        # Las siguientes lineas reciben el <name> correspondiente a cada <path> ubicado en el archivo <urls.py>, 
-        # el cual corresponde al metódo al que se está realizando testing, 
-        # metódo que está ubicado en el archivo <views.py>
+        #Producción
         self.search_production_url = reverse('searchproduction')
         self.render_create_production_url = reverse('createproduction')
         self.create_production_action_url = reverse('createproductionaction')
         self.update_production_action_url = reverse('updateproductionaction')
+        #Usuario
         self.search_user = reverse('searchuser')
         self.create_user_action = reverse('creatuseraction')
+        #Bovinos
+        self.search_bovine_url = reverse('searchbovine')
+        self.render_create_bovine_url = reverse('createbovine')
+        self.create_bovine_action_url = reverse('createbovineaction')
+        self.update_bovine_action_url = reverse('updatebovineaction')
+
+     #Testing para la búsquda de bovinos 
+     def test_search_bovine_GET(self):
+        response = self.client.get(self.search_bovine_url)
+        self.assertEquals(response.status_code, 200)
+        self.assertTemplateUsed(response, 'bovine/search.html')
+
+     #Testing para Render de crear bovinos
+     def test_render_create_bovine_GET(self):
+        response = self.client.get(self.render_create_bovine_url)
+        self.assertEquals(response.status_code, 200)
+        self.assertTemplateUsed(response, 'bovine/create.html')
+ 
+     #Testing para modificación de bovinos
+     def test_update_bovine_action_GET(self):
+        response = self.client.get(self.create_bovine_action_url)
+        self.assertEquals(response.status_code, 200)
+        self.assertTemplateUsed(response, 'bovine/create.html')
+     
+     #Testing para creación de bovinos
+     def test_create_bovine_action_GET(self):
+        response = self.client.get(self.update_bovine_action_url)
+        self.assertEquals(response.status_code, 200)
+        self.assertTemplateUsed(response, 'bovine/update.html')
 
     # Testing para Buscar Produccion
     def test_search_production_GET(self):
