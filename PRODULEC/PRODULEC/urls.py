871483278d21ca9349992495e5f215d11dbd39b8
Hernan Chain <hernan.chain01@gmail.com>
2020-02-16 22:55:48 -0500

    Setup root path as Initial page

--- PRODULEC/PRODULEC/urls.py
+++ PRODULEC/PRODULEC/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('produlect/', include('produlect.urls')),
+    path('', include('produlect.urls')),
     path('admin/', admin.site.urls),
 ]
